feca102032c2e565cd8e318955fc7c7816f2f7eb
kbodur <kbodur@pursuit.org>
2024-07-15 12:50:22 -0400

    added delete and update

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const {getAllSongs, createSong, getSong} = require('../queries/songs') 
+const { getAllSongs, createSong, getSong, deleteSong, updateSong } = require('../queries/songs') 
 const { checkName, checkArtist } = require("../validations/checkSongs");
 
 // INDEX
@@ -29,6 +29,27 @@ songs.get("/", async (req, res) => {
     const singleSong = await createSong(req.body);
     res.json(singleSong);
   });  
+
+
+// DELETE
+songs.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedSong = await deleteSong(id);
+    if (deletedSong.id) {
+      res.status(200).json({message: "Successfully deleted song."});
+    } else {
+      res.status(404).json({error: "Song not found"});
+    }
+  });
+  
+  // UPDATE
+  songs.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const updatedSong = await updateSong(id, req.body);
+    res.status(200).json(updatedSong);
+  });
+
+
 module.exports = songs;
 
 
--- queries/songs.js
+++ queries/songs.js
@@ -32,4 +32,29 @@ const createSong = async (song) => {
   }
 };
 
-module.exports = { getAllSongs, getSong, createSong };
+const deleteSong = async (id) => {
+  try {
+    const deletedSong = await db.one(
+      "DELETE FROM songs WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateSong = async (id, song) => {
+  try {
+    const updatedSong = await db.one(
+      "UPDATE songs SET name=$1, artist=$2, album=$3, time=$4, is_favorite=$5 where id=$6 RETURNING *",
+      [song.name, song.artist, song.album, song.time, song.is_favorite, id]
+    );
+    return updatedSong;
+  } catch (error) {
+    return error;
+  }
+};
+
+
+module.exports = { getAllSongs, getSong, createSong, deleteSong, updateSong };
